f8e59439540f5c915da0b826aac81d62838a1718
Your Name <hzn@google.com>
2018-06-18 21:46:41 -0400

    .

--- train.py
+++ train.py
@@ -115,32 +115,40 @@ def reduce_mem_usage(df):
     start_mem = df.memory_usage().sum() / 1024**2
     print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
 
+    simple = 0
+    int_long = 0
+    float_long = 0
+    object_type = 0
+
     for col in df.columns:
         col_type = df[col].dtype
-        print(col, ':', col_type)
 
         if col_type != object:
+            if col_type == 'bool' or col_type == 'int8' or col_type == 'int16':
+                simple += 1
+                continue
             c_min = df[col].min()
             c_max = df[col].max()
             if str(col_type)[:3] == 'int':
+                int_long += 1
                 if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
                     df[col] = df[col].astype(np.int8)
                 elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
                     df[col] = df[col].astype(np.int16)
                 elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
                     df[col] = df[col].astype(np.int32)
-                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)
             else:
+                assert('float' in str(col_type))
+                float_long += 1
                 if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
                     df[col] = df[col].astype(np.float16)
                 elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
                     df[col] = df[col].astype(np.float32)
-                else:
-                    df[col] = df[col].astype(np.float64)
         else:
-            print(col)
+            object_type += 1
+            print(col, ':', col_type)
 
+    print(simple, int_long, float_long, object_type, simple+int_long+float_long+object_type, len(df.columns))
     end_mem = df.memory_usage().sum() / 1024**2
     print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
     print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
