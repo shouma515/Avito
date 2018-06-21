f606faf528df6dbcb6c809f91ad189a2fa6b1894
hzn <benhzn07@gmail.com>
2018-06-21 15:02:34 -0400

    add catboost

---
+++ models/CatBoost.py
@@ -0,0 +1,27 @@
+from catboost import CatBoostRegressor
+
+class CatBoost():
+    def __init__(self, model_params = None, data_params = None):
+        self.model_params = model_params
+        self.data_params = data_params
+        self.model = CatBoostRegressor(**self.model_params)
+
+
+    def fit(self, X_train, y_train, X_val, y_val):
+        cat_features = self.data_params['categorical_feature']
+        # X_train.fillna(-999, inplace=True)
+        return self.model.fit(X_train, y_train, cat_features=list(cat_features), eval_set=[(X_val, y_val)])
+
+    def predict(self, X):
+        """ Predict on the given X, need to call fit first
+            Returns:
+                an array of the predict results, has the same rows as X.
+        """
+        # X.fillna(-999, inplace=True)
+        return self.model.predict(X)
+
+    def get_params(self):
+        return self.model_params if self.model_params is not None else {}
+
+    def get_features_importances(self):
+        return None
